7619888dc107c51646242b7c0a863637be97f1a1
yossyadirta <yossy.adirta@dansmultipro.com>
2023-11-06 19:33:29 +0700

    Setup init react-query

--- apps/container/src/components/SaforaApp.js
+++ apps/container/src/components/SaforaApp.js
@@ -1,61 +1,44 @@
-import React, { useRef, useEffect } from 'react'
-import { useHistory, Link } from 'react-router-dom'
-import { mount } from 'safora/Safora'
-// import {Button} from 'components/UI'
-import {get} from 'lodash'
-import { useDispatch, useSelector } from 'react-redux'
-import {fetchComments, commentsSelector, postsSelector} from 'components/Slices'
+import React, { useRef, useEffect } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import { mount } from 'safora/Safora';
+import { useDispatch, useSelector } from 'react-redux';
 
-const SaforaApp = ({ store }) => {
+const SaforaApp = ({ store, client }) => {
 	const ref = useRef(null);
-	const history = useHistory()
-	const dispatch = useDispatch()
-	const {
-	  comments,
-	  loading: commentsLoading,
-	  error: commentsHasErrors,
-	} = useSelector(commentsSelector)
-	console.log('comments', comments)
+	const history = useHistory();
+	const dispatch = useDispatch();
 
 	useEffect(() => {
-		dispatch(fetchComments(1))
-	}, [dispatch])
-
-	useEffect(() => {
-
 		const { onParentNavigation } = mount(ref.current, {
 			initialPath: history.location.pathname,
 			onParentNavigation: ({ pathname: nextPathname }) => {
-				const { pathname } = history.location
+				const { pathname } = history.location;
 
 				if (pathname !== nextPathname) {
-					history.push(nextPathname)
+					history.push(nextPathname);
 				}
 			},
 			dispatch,
 			selector: useSelector,
-			store: store
-		})
+			store: store,
+			client: client
+		});
 		if (onParentNavigation) {
-			history.listen(onParentNavigation)
+			history.listen(onParentNavigation);
 		}
-	}, [])
-	
-	const kucing = {
-		name: 'ceboi'
-	}
-	console.log(get(kucing, 'name'))
-	console.log(get(kucing, 'age', 'none'))
+	}, []);
+
 	return (
-		<div>
-			SaforaApp
-			total comments: {comments.length}
-			<br />
-			{/* <Button onClick={getApiProducts}>get api products</Button> */}
-			<Link to="/mea/list">GO TO MEA service</Link>
+		<div style={{ margin: '24px' }}>
+			<Link to="/" className="btn background-red">
+				GO TO SAFORA Home
+			</Link>
+			<Link to="/mea/list" className="btn background-blue">
+				GO TO MEA service
+			</Link>
 			<div ref={ref} />
 		</div>
-	)
-}
+	);
+};
 
-export default SaforaApp
+export default SaforaApp;
--- apps/safora/src/bootstrap.js
+++ apps/safora/src/bootstrap.js
@@ -1,34 +1,33 @@
-import React from "react";
-import ReactDom from 'react-dom'
-import App from "./App";
-import { createMemoryHistory, createBrowserHistory } from 'history'
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el, { onParentNavigation, defaultHistory, initialPath, selector, store }) => {
-	const history = defaultHistory || createMemoryHistory({
-		initialEntries: [initialPath]
-	})
-	if (onParentNavigation) history.listen(onParentNavigation)
-	ReactDom.render(
-		<App history={history} selector={selector} store={store} />,
-		el
-	)
+const mount = (el, { onParentNavigation, defaultHistory, initialPath, selector, store, client }) => {
+	const history =
+		defaultHistory ||
+		createMemoryHistory({
+			initialEntries: [ initialPath ]
+		});
+	if (onParentNavigation) history.listen(onParentNavigation);
+	ReactDom.render(<App history={history} selector={selector} store={store} client={client} />, el);
 	return {
 		onParentNavigation: ({ pathname: nextPathname }) => {
-			const { pathname } = history.location
+			const { pathname } = history.location;
 
 			if (pathname !== nextPathname) {
-				history.push(nextPathname)
+				history.push(nextPathname);
 			}
 		}
-	}
-}
+	};
+};
 
 if (process.env.NODE_ENV === 'development') {
-	const devRoot = document.querySelector('#safora')
+	const devRoot = document.querySelector('#safora');
 
 	if (devRoot) {
-		mount(devRoot, { defaultHistory: createBrowserHistory() })
+		mount(devRoot, { defaultHistory: createBrowserHistory() });
 	}
 }
 
-export { mount }
+export { mount };
